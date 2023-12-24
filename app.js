1c0be4a7580f8a4c474a3ea17c1b6c6748cd6870
Teosaka <121055393+Teosaka@users.noreply.github.com>
2023-12-23 16:20:26 -0800

    Stuff ain't working apparently supposed to work when in 'docs' folder? idk anyways also had to reinstall express for some reason

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const app = express();
 const DEFAULT_JS_LINTER_HATES_NUMBERS_PORT = 8000;
 
 app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(express.static('Docs'));
 const PORT = process.env.PORT || DEFAULT_JS_LINTER_HATES_NUMBERS_PORT;
 app.listen(PORT);
